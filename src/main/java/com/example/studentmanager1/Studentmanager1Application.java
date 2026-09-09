41b9d6420ff1a962387deb46ba1c43ea48b1967e
NgocNho-IT <dangngocnho285@gamil.com>
2026-09-09 14:47:13 +0700

    chore: initialize Spring Boot project with MVC architecture

---
+++ src/main/java/com/example/studentmanager1/Studentmanager1Application.java
@@ -0,0 +1,13 @@
+package com.example.studentmanager1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studentmanager1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Studentmanager1Application.class, args);
+	}
+
+}
